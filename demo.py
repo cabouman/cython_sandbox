2d751a2c55bf9aa2e955a814f8f91bcd9b8fcf95
Charles Bouman <bouman@purdue.edu>
2020-10-12 11:56:49 -0400

    install demo file

--- demo.py
+++ demo.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 
 import svmbir
 
+import os
+os.chdir("/Users/bouman/PycharmProjects/Data/demo-data")
 
-sino = np.load('data/sinodata.npy')
-weight = np.load('data/weightdata.npy')
+sino = np.load('sinodata.npy')
+weight = np.load('weightdata.npy')
 
 (num_views, num_slices, num_channels) = sino.shape
 angles = np.linspace(0, np.pi, num_views, endpoint=False)
@@ -24,18 +26,19 @@ imgplot = plt.imshow(x[0])
 imgplot.set_cmap('gray')
 plt.colorbar()
 
-plt.savefig('data/recon.png')
+plt.savefig('recon.png')
+plt.show()
 plt.close()
 
 imgplot = plt.imshow(np.swapaxes(sino, 0, 1)[0])
 imgplot.set_cmap("gray")
 plt.colorbar()
-plt.savefig("data/sino.png")
+plt.savefig("sino.png")
 plt.show()
 plt.close()
 
 imgplot = plt.imshow(np.swapaxes(sino-proj, 0, 1)[0])
 imgplot.set_cmap('gray')
 plt.colorbar()
-plt.savefig('data/err_sino.png')
+plt.savefig('err_sino.png')
 plt.close()
