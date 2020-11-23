842dc48f5dc4c00892d25095f483a57dbd5bda1b
DamonLee5 <liwr5damon@gmail.com>
2020-11-23 13:57:21 -0500

    Matrix multiplication.

---
+++ cython-sandbox/matrices/src/matrices.h
@@ -0,0 +1,15 @@
+#ifndef _MATRICES_H_
+#define _MATRICES_H_
+
+#include <stdio.h>
+
+struct Amatrix
+{
+    int NRows;
+    int NCols;
+    float **mat;           /* The array is indexed by mat[NRows][NCols] */
+};
+
+void matrix_multiplication(struct Amatrix *A, struct Amatrix *B , struct Amatrix *C);
+
+#endif
