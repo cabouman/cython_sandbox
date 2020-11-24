c8aa4a55521340911151cf72d85e9fe1590f603b
DamonLee5 <liwr5damon@gmail.com>
2020-11-23 19:03:20 -0500

    Updated python wrapper and demo.

--- cython_sandbox/wrapped.pyx
+++ cython_sandbox/wrapped.pyx
@@ -2,10 +2,7 @@ cimport cython
 import numpy as np
 cimport numpy as np
 import ctypes
-from numpy import int32, float
-from numpy cimport int32_t, float_t
 from libc.stdlib cimport malloc, free
-
 cimport matrices
 from matrices cimport Amatrix, matrix_multiplication
 
@@ -13,50 +10,44 @@ from matrices cimport Amatrix, matrix_multiplication
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def py_matrix_multiplication( np.ndarray[np.float_t,ndim=2,mode='c'] mat,np.ndarray[np.float_t,ndim=2,mode='c'] mat1):
+def py_matrix_multiplication(np.ndarray[np.float_t,ndim=2,mode='c'] py_a, np.ndarray[np.float_t, ndim=2, mode='c'] py_b):
     cdef int i
-    row_size,column_size = np.shape(mat)
-    row_size1,column_size1 = np.shape(mat1)
-    cdef np.ndarray[float, ndim=2, mode="c"] temp_mat = np.ascontiguousarray(mat, dtype = ctypes.c_float)
-    cdef np.ndarray[float, ndim=2, mode="c"] temp_mat1 = np.ascontiguousarray(mat1, dtype = ctypes.c_float)
-    # cdef np.npy_intp shape[2]
+    nrows_a,ncols_a = np.shape(py_a)
+    nrows_b,ncols_b = np.shape(py_b)
+    cdef np.ndarray[float, ndim=2, mode="c"] temp_a = np.ascontiguousarray(py_a, dtype = ctypes.c_float)
+    cdef np.ndarray[float, ndim=2, mode="c"] temp_b = np.ascontiguousarray(py_b, dtype = ctypes.c_float)
+
     cdef Amatrix A
-    A.NRows = row_size
-    A.NCols = column_size
-    A.mat = <float **>malloc(column_size * sizeof(float*))
+    A.NRows = nrows_a
+    A.NCols = ncols_a
+    A.mat = <float **>malloc(ncols_a * sizeof(float*))
 
     cdef Amatrix B
-    B.NRows = row_size1
-    B.NCols = column_size1
-    B.mat = <float **>malloc(column_size * sizeof(float*))
+    B.NRows = nrows_b
+    B.NCols = ncols_b
+    B.mat = <float **>malloc(ncols_a * sizeof(float*))
 
     cdef Amatrix C
     if not A.mat:
         raise MemoryError
     try:
-        for i in range(row_size):
-            A.mat[i] = &temp_mat[i, 0]
-        for i in range(row_size1):
-            B.mat[i] = &temp_mat1[i, 0]
+        for i in range(nrows_a):
+            A.mat[i] = &temp_a[i, 0]
+        for i in range(nrows_b):
+            B.mat[i] = &temp_b[i, 0]
 
         matrix_multiplication(&A, &B, &C)
         mat2 = np.zeros((C.NRows,C.NCols))
 
-        # shape[0] = <np.npy_intp> C.NRows
-        # shape[1] = <np.npy_intp> C.NCols
+        for i in range(C.NRows):
+            for j in range(C.NCols):
+                mat2[i][j]=C.mat[i][j]
+        return mat2
 
-        # ndarray = np.PyArray_SimpleNewFromData(2, shape,
-        #             np.NPY_FLOAT, <void *> C.mat)
-        # np.PyArray_UpdateFlags(ndarray, ndarray.flags.num | np.NPY_OWNDATA)
-        # for i in range(C.NRows):
-        #     for j in range(C.NCols):
-        #         mat2[i][j]=C.mat[i][j]
-        # return mat2
-        # return np.array(np.ascontiguousarray(C.mat))
     finally:
         print("a")
-        # free(A)
-        # free(B)
-        # free(C)
+        free(A.mat)
+        free(B.mat)
+        free(C.mat)
 
 
--- demo/demo.py
+++ demo/demo.py
@@ -1,14 +1,14 @@
 import numpy as np
 from cython_sandbox import py_matrix_multiplication
 import time
-A=np.random.randint(10,size=(130,40)).astype(np.float)
-B=np.random.randint(10,size=(40,150)).astype(np.float)
+A=np.random.randint(10,size=(3,4)).astype(np.float)
+B=np.random.randint(10,size=(4,5)).astype(np.float)
 t = time.time()
-py_matrix_multiplication(A,B)
-#print(C)
+C = py_matrix_multiplication(A,B)
+print(C)
 print("cython finished : %f s"%(time.time() - t))
 
 t = time.time()
-C=np.dot(A,B)
-#print()
+C = np.dot(A,B)
+print(C)
 print("numpy  finished : %f s"%(time.time() - t))
