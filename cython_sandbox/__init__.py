cb999ea00fa1ed2ebfce813136ea60c557995417
Soumendu Majee <soumendu.fb9@gmail.com>
2020-12-09 18:37:40 -0500

    add helpful comments to init file

--- cython_sandbox/__init__.py
+++ cython_sandbox/__init__.py
@@ -1,1 +1,3 @@
+# import the function 'py_matrix_multiplication' from the package 'wrapped' 
+# from the cython file 'wrapped.pyx' in the current directory (.)
 from .wrapped import py_matrix_multiplication
