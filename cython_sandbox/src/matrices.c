374d287ac67ef236d92377171b176ba1c9bf6548
Charles Bouman <Charles.Bouman@gmail.com>
2020-12-11 17:46:45 -0500

    added pragma directive

--- cython_sandbox/src/matrices.c
+++ cython_sandbox/src/matrices.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 
 #include "allocate.h"
 #include "matrices.h"
@@ -16,6 +17,7 @@ int matrix_multiplication(struct matrix_float *A, struct matrix_float *B , struc
 
     /* Compute matrix product */
     for (i = 0; i < A->NRows ; i ++ )
+        #pragma omp parallel for
         for (j = 0; j < B->NCols ; j ++ ) {
             C->mat[i][j] = 0;
             for (k = 0; k < A->NCols ; k ++ )
